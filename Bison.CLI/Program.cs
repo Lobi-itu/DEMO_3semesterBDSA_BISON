85cd82d5ed574ea225c45b41c8834fedbb02f0f7
Lobi-itu <lobi@itu.dk>
2026-09-09 14:22:36 +0200

    Refactor main to print CLI arguments.
    Co-authored-by: Eduard <edka@itu.dk>

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -1,7 +1,2 @@
-﻿if (args[0] == "say")
-{
- var message = args[1];
- var frequency = int.Parse(args[2]);
- foreach (var i in Enumerable.Range(1, frequency))
- Console.Write(message + " ");
-}
+﻿foreach (var arg in args)
+ Console.WriteLine(arg);
